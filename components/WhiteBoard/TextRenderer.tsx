16ff1d9235025a5b65b2ed9367b7b2914f0517e2
junhea <97426534+junhea@users.noreply.github.com>
2023-07-13 16:22:51 +0900

    ✨ feat: create TextRenderer component
    textObj를 렌더링 해주는 컴포넌트
    textGeometry보다 drei의 Text가 훨씬 좋다

---
+++ components/WhiteBoard/TextRenderer.tsx
@@ -0,0 +1,22 @@
+import { Text } from '@react-three/drei';
+interface TextViewProps {
+  obj: TextObj;
+}
+
+export default function TextRenderer({ obj }: TextViewProps) {
+  return (
+    <Text
+      font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+      fontSize={obj.fontSize}
+      maxWidth={obj.w}
+      color={obj.color}
+      overflowWrap={obj.overflow}
+      lineHeight={1}
+      position={[obj.x, obj.y, obj.depth]}
+      anchorX={'center'}
+      anchorY={'middle'}
+    >
+      {obj.text}
+    </Text>
+  );
+}
