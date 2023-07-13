cca5f79e23912a1145c4197ef849dcc7b83db2bc
junhea <97426534+junhea@users.noreply.github.com>
2023-07-13 16:21:04 +0900

    ✨ feat: create RectangleRenderer component
    RectObj를 렌더링 해주는 컴포넌트

---
+++ components/WhiteBoard/RectangleRenderer.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { objState } from '@/states/whiteboard';
+import { useRecoilState } from 'recoil';
+
+interface Props {
+  obj: RectObj;
+}
+
+// renders given rectangle object, sets selection onClick
+export default function RectangleRenderer({ obj }: Props) {
+  const [selection, setSelection] = useRecoilState(objState);
+  return (
+    <mesh
+      position={[obj.x, obj.y, obj.depth]}
+      scale={1}
+      onClick={(e) => {
+        e.stopPropagation();
+        setSelection(obj.objId);
+      }}
+    >
+      <planeGeometry attach={'geometry'} args={[obj.w, obj.h]} />
+      <meshStandardMaterial color={obj.color} depthWrite={true} depthTest={true} />
+    </mesh>
+  );
+}
