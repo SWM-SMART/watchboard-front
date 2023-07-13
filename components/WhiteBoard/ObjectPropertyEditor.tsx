74b9556f9495ee7669d206f691e1a78fd4b95a3e
junhea <97426534+junhea@users.noreply.github.com>
2023-07-13 16:00:03 +0900

    ✨ feat: create ObjectPropertyEditor component

---
+++ components/WhiteBoard/ObjectPropertyEditor.tsx
@@ -0,0 +1,55 @@
+import { objMapState } from '@/states/whiteboard';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+
+interface ObjectPropertyEditorProps {
+  targetObjId: string;
+}
+
+// view and edit target object properties
+export default function ObjectPropertyEditor({ targetObjId }: ObjectPropertyEditorProps) {
+  const [objMap, setObjMap] = useRecoilState(objMapState);
+  const [obj, setObj] = useState<Obj | undefined>(undefined);
+
+  // get object from objMap using targetObjId
+  useEffect(() => {
+    if (targetObjId === null) return;
+    setObj(objMap.get(targetObjId));
+  }, [targetObjId, objMap]);
+
+  // update objMap if obj value has changed
+  useEffect(() => {
+    if (obj === undefined) return;
+    setObjMap((m) => {
+      return new Map([...m, [obj.objId, obj]]);
+    });
+  }, [obj, setObjMap]);
+
+  // show nothing if obj is not found or set
+  if (obj === undefined) return <></>;
+
+  // save object properties in array
+  const keys = Object.keys(obj);
+  const values = Object.values(obj);
+
+  return (
+    <div>
+      {values.map((v, i) => {
+        return (
+          <div key={`objectviewer-${keys[i]}`}>
+            <span>{keys[i]}</span>
+            <input
+              onChange={(e) => {
+                setObj((o) => {
+                  if (o === undefined) return undefined;
+                  return { ...o, [keys[i]]: e.target.value };
+                });
+              }}
+              value={v}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
