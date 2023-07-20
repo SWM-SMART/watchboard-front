6d801c781260e7849d520cd1842558aac0952593
junhea <97426534+junhea@users.noreply.github.com>
2023-07-20 15:27:19 +0900

    ✨ feat: create http module

---
+++ utils/http.ts
@@ -0,0 +1,13 @@
+export async function httpGet(url: string) {
+  return await fetch(url, { method: 'GET' });
+}
+
+export async function httpPost(url: string, body: any) {
+  return await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+}
