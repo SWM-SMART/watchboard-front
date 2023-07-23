48a58ded70a77065064155380e19db1614109f4b
junhea <97426534+junhea@users.noreply.github.com>
2023-07-23 14:14:40 +0900

    💄 feat: use google inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import RecoilRootWrapper from '@/components/RecoilRootWrapper';
 import './globals.css';
+import { Inter } from 'next/font/google';
 import LoadMsw from './LoadMsw';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body>
+      <body className={inter.className}>
         <LoadMsw />
         <RecoilRootWrapper>{children}</RecoilRootWrapper>
       </body>
