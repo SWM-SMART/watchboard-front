d333ace98ff8fa24744317af0f932abbe4c3840a
junhea <97426534+junhea@users.noreply.github.com>
2023-07-24 12:42:43 +0900

    💥 feat: BackgroundButton is now ssc

--- components/BackgroundButton/index.tsx
+++ components/BackgroundButton/index.tsx
@@ -1,22 +1,23 @@
-'use client';
-import { MouseEventHandler } from 'react';
 import styles from './backgroundButton.module.css';
 import 'material-symbols';
+import Link from 'next/link';
 
 interface BackgroundButtonProps {
   text: string;
   icon?: string;
   invert?: boolean;
-  onClick: MouseEventHandler;
+  href: string;
 }
 
-export default function BackgroundButton({ text, icon, invert, onClick }: BackgroundButtonProps) {
+export default function BackgroundButton({ text, icon, invert, href }: BackgroundButtonProps) {
   return (
-    <div className={`${styles.container} ${invert ? styles.invert : null}`} onClick={onClick}>
-      {icon === undefined ? null : (
-        <span className={`material-symbols-outlined ${styles.icon}`}>{icon}</span>
-      )}
-      <span className={styles.text}>{text}</span>
-    </div>
+    <Link href={href} className={styles.link}>
+      <div className={`${styles.container} ${invert ? styles.invert : null}`}>
+        {icon === undefined ? null : (
+          <span className={`material-symbols-outlined ${styles.icon}`}>{icon}</span>
+        )}
+        <span className={styles.text}>{text}</span>
+      </div>
+    </Link>
   );
 }
