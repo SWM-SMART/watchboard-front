8ede6cf0427064f39ab30808ffe8bc2d0bd1df05
junhea <97426534+junhea@users.noreply.github.com>
2023-07-24 14:32:58 +0900

    ✨ feat: add cookie parameter to api calls
    server component에서 fetch시 cookie를 전달 해주기 위함

--- utils/api.ts
+++ utils/api.ts
@@ -1,19 +1,30 @@
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 import { httpDelete, httpGet, httpPost } from './http';
 
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export async function getDocumentList(): Promise<WBDocumentListReponse> {
-  return (await httpGet(`${API_BASE_URL}/documents`)).json();
+export async function getDocumentList(
+  cookieStore?: ReadonlyRequestCookies,
+): Promise<WBDocumentListReponse> {
+  return (await httpGet(`${API_BASE_URL}/documents`, cookieStore)).json();
 }
 
-export async function getDocument(document_id: number): Promise<WBDocumentReponse> {
-  return (await httpGet(`${API_BASE_URL}/documents/${document_id}`)).json();
+export async function getDocument(
+  document_id: number,
+  cookieStore?: ReadonlyRequestCookies,
+): Promise<WBDocumentReponse> {
+  return (await httpGet(`${API_BASE_URL}/documents/${document_id}`, cookieStore)).json();
 }
 
-export async function createDocument(document_name: string): Promise<WBDocumentCreateResponse> {
-  return (await httpPost(`${API_BASE_URL}/documents`, { document_name: document_name })).json();
+export async function createDocument(
+  document_name: string,
+  cookieStore?: ReadonlyRequestCookies,
+): Promise<WBDocumentCreateResponse> {
+  return (
+    await httpPost(`${API_BASE_URL}/documents`, { document_name: document_name }, cookieStore)
+  ).json();
 }
 
-export async function deleteDocument(document_id: number) {
-  await httpDelete(`${API_BASE_URL}/documents/${document_id}`);
+export async function deleteDocument(document_id: number, cookieStore?: ReadonlyRequestCookies) {
+  await httpDelete(`${API_BASE_URL}/documents/${document_id}`, cookieStore);
 }
