fb4a467c942dd2f57d4dc4ae184ea3a10fb46324
junhea <97426534+junhea@users.noreply.github.com>
2023-07-27 13:07:39 +0900

    ✨ feat: reimplement ToolSelector
    대부분 스타일 관련 수정

---
+++ components/WhiteBoard/ToolSelector/index.tsx
@@ -0,0 +1,33 @@
+'use client';
+import styles from './toolSelector.module.css';
+import 'material-symbols';
+import { currentToolState } from '@/states/whiteboard';
+import { useRecoilState } from 'recoil';
+
+export default function ToolSelector() {
+  return (
+    <div className={styles.container}>
+      <ToolButton toolName={'HAND'} icon={'drag_pan'} />
+      <ToolButton toolName={'SELECT'} icon={'select'} />
+      <ToolButton toolName={'RECT'} icon={'rectangle'} />
+      <ToolButton toolName={'TEXT'} icon={'insert_text'} />
+    </div>
+  );
+}
+
+interface ToolButtonProps {
+  toolName: Tool;
+  icon: string;
+}
+
+function ToolButton({ toolName, icon }: ToolButtonProps) {
+  const [tool, setTool] = useRecoilState(currentToolState);
+  return (
+    <div
+      className={`${styles.buttonContainer} ${tool === toolName ? styles.buttonSelection : ''}`}
+      onClick={() => setTool(toolName)}
+    >
+      <span className={`material-symbols-outlined ${styles.icon}`}>{icon}</span>
+    </div>
+  );
+}
