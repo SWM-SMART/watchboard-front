d980d627a6957fb1c155a7eeda56366f1f4cb916
junhea <97426534+junhea@users.noreply.github.com>
2023-08-07 14:20:33 +0900

    ✨ feat: ClickableBackgroundButton

---
+++ components/BackgroundButton/ClickableBackgroundButton.tsx
@@ -0,0 +1,27 @@
+'use client';
+import styles from './backgroundButton.module.css';
+import 'material-symbols';
+import { MouseEventHandler } from 'react';
+
+interface ClickableBackgroundButtonProps {
+  text: string;
+  icon?: string;
+  invert?: boolean;
+  onClick?: MouseEventHandler;
+}
+
+export default function ClickableBackgroundButton({
+  text,
+  icon,
+  invert,
+  onClick,
+}: ClickableBackgroundButtonProps) {
+  return (
+    <div onClick={onClick} className={`${styles.container} ${invert ? styles.invert : null}`}>
+      {icon === undefined ? null : (
+        <span className={`material-symbols-outlined ${styles.icon}`}>{icon}</span>
+      )}
+      <span className={styles.text}>{text}</span>
+    </div>
+  );
+}
