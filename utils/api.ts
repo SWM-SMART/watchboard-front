1ec56290345ebddd707149b227dec66e810d4825
junhea <97426534+junhea@users.noreply.github.com>
2023-08-08 15:16:25 +0900

    🐛 fix: silent retry + dispatch custom error event

--- utils/api.ts
+++ utils/api.ts
@@ -4,15 +4,15 @@ export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const KAKAO_AUTH_URL = `${API_BASE_URL}/oauth2/authorization/kakao`;
 
 export async function getDocumentList(): Promise<WBDocumentListReponse> {
-  return (await httpGet(`${API_BASE_URL}/documents`)).json();
+  return (await httpGet(`${API_BASE_URL}/documents`))?.json();
 }
 
 export async function getDocument(documentId: number): Promise<WBDocumentReponse> {
-  return (await httpGet(`${API_BASE_URL}/documents/${documentId}`)).json();
+  return (await httpGet(`${API_BASE_URL}/documents/${documentId}`))?.json();
 }
 
 export async function createDocument(documentName: string): Promise<WBDocumentCreateResponse> {
-  return (await httpPost(`${API_BASE_URL}/documents`, { documentName: documentName })).json();
+  return (await httpPost(`${API_BASE_URL}/documents`, { documentName: documentName }))?.json();
 }
 
 export async function deleteDocument(documentId: number) {
@@ -20,9 +20,9 @@ export async function deleteDocument(documentId: number) {
 }
 
 export async function getUserData(): Promise<UserDataResponse> {
-  return (await httpGet(`${API_BASE_URL}/users/info`)).json();
+  return (await httpGet(`${API_BASE_URL}/users/info`))?.json();
 }
 
 export async function refreshToken(): Promise<string | null> {
-  return (await httpGet(`${API_BASE_URL}/users/token`, false)).headers.get('Authorization');
+  return (await httpGet(`${API_BASE_URL}/users/token`, false))?.headers.get('Authorization') || '';
 }
--- utils/http.ts
+++ utils/http.ts
@@ -1,54 +1,40 @@
+'use client';
 import { useUser } from '@/states/user';
 import { refreshToken } from './api';
+import { throwError } from './ui';
 
 // refresh token on failure
 export async function httpGet(url: string, retry: boolean = true) {
   const headers = createHeaders();
-  const res = await fetch(url, {
-    method: 'GET',
-    credentials: 'include',
-    headers: headers,
-  });
-  if (res.status >= 400 && retry) {
-    await preRetry(headers);
-    const newres = await fetch(url, {
+  const getRes = (headers: Headers) =>
+    fetch(url, {
       method: 'GET',
       credentials: 'include',
       headers: headers,
     });
-    return newres;
-  }
-  return res;
+
+  return doGetRes(getRes, headers, retry);
 }
 
 export async function httpPost(url: string, body: any, retry: boolean = true) {
   const headers = createHeaders();
-  const res = await fetch(url, {
-    method: 'POST',
-    credentials: 'include',
-    headers: headers,
-    body: JSON.stringify(body),
-  });
-  if (res.status >= 400 && retry) {
-    await preRetry(headers);
-    return await fetch(url, {
+  const getRes = (headers: Headers) =>
+    fetch(url, {
       method: 'POST',
       credentials: 'include',
       headers: headers,
       body: JSON.stringify(body),
     });
-  }
-  return res;
+
+  return doGetRes(getRes, headers, retry);
 }
 
 export async function httpDelete(url: string, retry: boolean = true) {
   const headers = createHeaders();
-  const res = await fetch(url, { method: 'DELETE', credentials: 'include', headers: headers });
-  if (res.status >= 400 && retry) {
-    await preRetry(headers);
-    return await fetch(url, { method: 'DELETE', credentials: 'include', headers: headers });
-  }
-  return res;
+  const getRes = (headers: Headers) =>
+    fetch(url, { method: 'DELETE', credentials: 'include', headers: headers });
+
+  return doGetRes(getRes, headers, retry);
 }
 
 function createHeaders() {
@@ -59,7 +45,27 @@ function createHeaders() {
 }
 
 async function preRetry(headers: Headers) {
-  const newAccessToken = (await refreshToken()) || '';
+  const newAccessToken = await refreshToken();
+  if (newAccessToken === null || newAccessToken.length === 0) return false;
   headers.set('Authorization', newAccessToken);
-  useUser.setState((state) => ({ accessToken: newAccessToken }));
+  useUser.setState(() => ({ accessToken: newAccessToken }));
+  return true;
+}
+
+async function doGetRes(
+  getRes: (headers: Headers) => Promise<Response>,
+  headers: Headers,
+  retry: boolean,
+) {
+  try {
+    return await getRes(headers);
+  } catch (e) {
+    if (!retry) {
+      if (e instanceof Error) throwError(e.message);
+      return;
+    }
+    if ((await preRetry(headers)) === false) return;
+  }
+
+  return await getRes(headers);
 }
