d39e716408258a7017ce5431bd78aaf6c8c21907
junhea <97426534+junhea@users.noreply.github.com>
2023-10-31 00:44:01 +0900

    ✨ feat: keywords are now categorized + can sync
    적용되지 않은 변경사항과
    기존의 키워드로 분리됨
    변경사항이 존재하면 동기화 요청을 서버로 보낼 수 있음

--- components/DataViewer/OptionPanel/index.tsx
+++ components/DataViewer/OptionPanel/index.tsx
@@ -1,8 +1,10 @@
-import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useMemo, useState } from 'react';
 import styles from './optionPanel.module.css';
 import 'material-symbols';
 import { useViewer } from '@/states/viewer';
 import SmallIconButton from '@/components/Button/SmallIconButton';
+import ClickableBackgroundButton from '@/components/BackgroundButton/ClickableBackgroundButton';
+import { updateKeywords } from '@/utils/api';
 
 const DEFAULT_WIDTH = 350;
 
@@ -13,11 +15,16 @@ interface OptionPanelProps {
 export default function OptionPanel({ children }: OptionPanelProps) {
   const [width, setWidth] = useState<number>(DEFAULT_WIDTH);
   const [drag, setDrag] = useState<boolean>(false);
-  const { keywords, currentTool, setCurrentTool } = useViewer((state) => ({
-    keywords: state.keywords,
-    currentTool: state.currentTool,
-    setCurrentTool: state.setCurrentTool,
-  }));
+  const [syncInProgress, setSyncInProgress] = useState<boolean>(false);
+  const { keywords, currentTool, setCurrentTool, documentId, syncDocument, clearSyncDocument } =
+    useViewer((state) => ({
+      keywords: state.keywords,
+      currentTool: state.currentTool,
+      setCurrentTool: state.setCurrentTool,
+      documentId: state.document?.documentId,
+      syncDocument: state.syncDocument,
+      clearSyncDocument: state.clearSyncDocument,
+    }));
 
   useEffect(() => {
     if (!drag) return;
@@ -32,13 +39,42 @@ export default function OptionPanel({ children }: OptionPanelProps) {
     };
   }, [drag]);
 
-  const keys = Array.from(keywords.keys());
+  const [stableKeys, addedKeys, removedKeys] = useMemo(() => {
+    const stable: string[] = [];
+    const added: string[] = [];
+    const removed: string[] = [];
+    for (const [k, v] of keywords.entries()) {
+      if (v.type === 'STABLE') stable.push(k);
+      else if (v.type === 'ADD') added.push(k);
+      else if (v.type === 'DELETE') removed.push(k);
+    }
+    return [stable, added, removed];
+  }, [keywords]);
+
+  const syncKeywords = () => {
+    if (documentId === undefined) return;
+    setSyncInProgress(true);
+    (async () => {
+      // clear prev document
+      clearSyncDocument();
+      // sync local changes
+      await updateKeywords(documentId, addedKeys, removedKeys);
+      // wait for sync to finish
+      // TODO: implement this
+      setTimeout(() => {
+        // done!
+        setSyncInProgress(false);
+        // reload document
+        syncDocument();
+      }, 3000);
+    })();
+  };
 
   return (
     <div className={styles.container} style={{ width: `${width}px` }}>
       <div
         className={styles.handle}
-        onPointerDown={(e) => {
+        onPointerDown={() => {
           setDrag(true);
         }}
       >
@@ -60,12 +96,51 @@ export default function OptionPanel({ children }: OptionPanelProps) {
             />
           </div>
         </div>
-        <div className={styles.keywords}>
-          {keys.map((v) => (
-            <KeywordView key={v} label={v} />
-          ))}
+        <div className={styles.keywordsContainer}>
+          <div
+            className={styles.keywords}
+            style={syncInProgress ? { pointerEvents: 'none', opacity: '0.5' } : undefined}
+          >
+            <KeywordHeader label={`변경 사항 (${addedKeys.length + removedKeys.length})`}>
+              {addedKeys.length + removedKeys.length > 0 ? (
+                <ClickableBackgroundButton
+                  text={'변경 사항 적용'}
+                  icon={'sync'}
+                  onClick={syncKeywords}
+                />
+              ) : (
+                <></>
+              )}
+            </KeywordHeader>
+            {addedKeys.map((v) => (
+              <KeywordView key={v} label={v} />
+            ))}
+            {removedKeys.map((v) => (
+              <KeywordView key={v} label={v} />
+            ))}
+          </div>
+          <div className={styles.keywords}>
+            <KeywordHeader label={`키워드 (${stableKeys.length})`} />
+            {stableKeys.map((v) => (
+              <KeywordView key={v} label={v} />
+            ))}
+          </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+interface KeywordHeaderProps {
+  label: string;
+  children?: ReactNode;
+}
+
+function KeywordHeader({ label, children }: KeywordHeaderProps) {
+  return (
+    <div className={styles.keywordHeaderContainer}>
+      <p className={styles.keywordsHeader}>{label}</p>
+      <div className={styles.keywordHeaderActions}>{children}</div>
     </div>
   );
 }
@@ -75,15 +150,17 @@ interface KeywordViewProps {
 }
 
 function KeywordView({ label }: KeywordViewProps) {
-  const { enabled, setKeyword } = useViewer((state) => ({
-    enabled: state.keywords.get(label),
+  const { state, setKeyword } = useViewer((state) => ({
+    state: state.keywords.get(label),
     setKeyword: state.setKeyword,
   }));
 
+  if (state === undefined) return <></>;
+
   return (
     <div
-      className={`${styles.keyword} ${enabled ? styles.enabled : ''}`}
-      onClick={() => setKeyword(label, !enabled)}
+      className={`${styles.keyword} ${state.enabled ? styles.enabled : ''}`}
+      onClick={() => setKeyword(label, !state.enabled)}
     >
       {label}
     </div>
