d35ac64557440736c024b66134d68b0e49d352ce
junhea <97426534+junhea@users.noreply.github.com>
2023-11-17 15:30:02 +0900

    feat: redirect to landing page if no session

--- app/components/DocumentList.tsx
+++ app/components/DocumentList.tsx
@@ -2,13 +2,20 @@
 import { useDocument } from '../../states/document';
 import { useEffect } from 'react';
 import Card from '../../components/Card';
+import { useUser } from '@/states/user';
+import { useRouter } from 'next/navigation';
 
 export default function DocumentList() {
   const { documentList, fetchDocumentList } = useDocument();
+  const userData = useUser((state) => state.userData);
+  const router = useRouter();
 
   useEffect(() => {
     fetchDocumentList();
   }, [fetchDocumentList]);
+
+  // redirect to landing page if not logged in
+  if (userData === null) router.push('/landing');
 
   return documentList?.map((document) => {
     return <Card key={`document-card-${document.documentId}`} document={document} />;
